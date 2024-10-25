078aa8bd4a61b25b7a9d77ef5e978772cc2b4b87
Yogesh Deshpande <yogesh.deshpande@arm.com>
2024-10-25 12:03:23 +0100

    [WIP] Add the Intel types
    Signed-off-by: Yogesh Deshpande <yogesh.deshpande@arm.com>

--- comid/tdx-profile/types.go
+++ comid/tdx-profile/types.go
@@ -2,12 +2,15 @@ package tdx
 
 import "github.com/veraison/corim/comid"
 
+// TO DO, check change this later to more expandable type
+type numericType uint
+
 type teeModel string
 type teeVendor string
 
 type pceID string
 
-type teeSVN int
+type teeSVN numericType
 
 type maskType []byte
 
@@ -20,8 +23,8 @@ type epochSeconds int
 type setType any
 
 type epochExpression struct {
-	grace_period epochSeconds
-	epochID      *tdate
+	gracePeriod epochSeconds
+	epochID     *tdate
 }
 
 type teeTcbStatus setType
@@ -32,11 +35,20 @@ type teeTcbCompSvn [16][16]teeSVN
 
 type teeMiscSelect maskType
 
-// TO DO Align later, this is with real TD profile
+type teeAtttributes maskType
+
+// TO DO Check with Ned, why it is NOT UUID but either an Integer or Bstr in the Profile Document
 type teeIsvProdID comid.UUID
 
-type teeInstanceID []byte
+// TO DO Change this Instance ID to be a type choice with expression for a []byte
+type teeInstanceID uint
 
 type teeCryptoKey comid.CryptoKey
 
 type teeAdvisoryID setType
+
+type epochTimeStamp tdate
+
+// TO DO Set of Set Type: Where it is used and is it needed, for this profile ...?
+
+// TO DO Check with Ned, What is time? in the CDDL Document, not defined???
